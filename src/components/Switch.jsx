385607f6055514d5ce9f2870be4f18e29407c273
fin <2581613136@qq.com>
2025-09-17 17:54:23 +0800

    fix: back to start

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -6,14 +6,13 @@ export default function Switch({ label, defaultOn, onChange }) {
     const newOn = !on;
     setOn(newOn);
 
-    // 调用父组件传递的回调函数，将开关状态传递给父组件
     if (onChange) {
       onChange(newOn);
     }
   };
   return (
     <div className="switch-container">
-    <p className="switch-label">{label}</p>
+      <p className="switch-label">{label}</p>
       <div className={`switch ${on ? "on" : "off"}`} onClick={handleToggle}>
         {/* <span className="label">{label}</span> */}
         <div className={`slider ${on ? "on" : "off"}`} />
